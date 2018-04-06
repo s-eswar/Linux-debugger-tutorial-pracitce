509102fd589e98441b671270b9b57e4930592130
S.Eswar <26323541+s-eswar@users.noreply.github.com>
2018-04-06 11:13:03 +0530

    sample test Program

---
+++ hello_world-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void first_function(void);
+void second_function(int);
+   
+int main(void)
+{
+   printf("hello world\n");
+   first_function();
+   printf("goodbye goodbye\n");
+   return 0;
+}
+  
+void first_function(void)
+{
+   int imidate = 3;
+   second_function(imidate);
+   imidate = 10;
+}
+  
+void second_function(int a)
+{
+    int b = a;
+}
